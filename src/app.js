0bc8e1d8a4079286dfa13d78ae8da624f45ec432
IndudiniThennakoon <thennakoonindudini@gmail.com>
2022-12-23 13:21:36 +0530

    adde some spaces

--- src/app.js
+++ src/app.js
@@ -5,5 +5,6 @@ const customer = require('./routes/customerRoute')
 
 app.use(router.routes()).use(router.allowedMethods()); // registering routes to the application
 app.use(customer.routes()).use(customer.allowedMethods()); // registering routes to the application
+
 app.listen(2400, () => console.log('Server running at PORT 2400'))
 
